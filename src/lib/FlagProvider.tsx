179a1dd3ac1ef862457ce59fe37d83a5fbf96b2f
Mark Lyck <mark.lyck@gmail.com>
2021-01-20 21:03:58 -0500

    small fixes

--- src/lib/FlagProvider.tsx
+++ src/lib/FlagProvider.tsx
@@ -1,21 +1,13 @@
-import React, { useEffect } from 'react'
-import flagsmith from 'flagsmith';
-
-import { useAtom, flagAtom } from 'src/atoms'
+import { useEffect } from 'react'
+import flagsmith from 'flagsmith/isomorphic';
 
 const FlagProvider = ({ children }: any) => {
-    const [flags] = useAtom(flagAtom)
-    console.log("flags", flags)
-
     useEffect(() => {
+        console.log('useEffect')
         flagsmith.init({
             environmentID: "8VKHezCZtcCNPfcTmtjddh",
             cacheFlags: true,
-            onChange: (oldFlags: any, params: any) => {
-                console.log("params", params)
-                console.log("soldFlags", oldFlags)
-                console.log("flags onChange", flagsmith.getFlags())
-            }
+            onChange: () => { }
         });
     }, [])
 
