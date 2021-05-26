67bd401f440b0d0c1aec0daeacb9205918521449
MarkLyck <mark.lyck@gmail.com>
2021-05-26 15:56:20 -0500

    :label: fix unused type

--- src/components/Dashboard/Account/ManageSubscription/Cancel/Modal/Success.tsx
+++ src/components/Dashboard/Account/ManageSubscription/Cancel/Modal/Success.tsx
@@ -16,21 +16,27 @@ const Container = styled.div`
     font-size: 2rem;
   }
 `
-const Success = ({ cancelReason, onModalDismiss }: { cancelReason: string; onModalDismiss: () => void }) => (
-  <Container>
-    <FontAwesomeIcon icon={['fas', 'tags']} />
-    <Title>Your subscription is cancelled</Title>
-    <Paragraph>
-      If you bought any of our signals, we strongly recommend you hold onto them until we send out our SELL signals for
-      them.
-    </Paragraph>
-    <Paragraph>
-      We publish all of our SELL signals publicly, so you can always check back to our home page to see what we ended up
-      selling that stock pick for.
-    </Paragraph>
-    <Paragraph>From the team at Weekly Stocktip, we wish you good luck in your future investment endeavors!</Paragraph>
-    <Button onClick={onModalDismiss}>Return to My Account</Button>
-  </Container>
-)
+const Success = ({ cancelReason, onModalDismiss }: { cancelReason: string; onModalDismiss: () => void }) => {
+  console.log('🔈 ~ cancelReason', cancelReason)
+
+  return (
+    <Container>
+      <FontAwesomeIcon icon={['fas', 'tags']} />
+      <Title>Your subscription is cancelled</Title>
+      <Paragraph>
+        If you bought any of our signals, we strongly recommend you hold onto them until we send out our SELL signals
+        for them.
+      </Paragraph>
+      <Paragraph>
+        We publish all of our SELL signals publicly, so you can always check back to our home page to see what we ended
+        up selling that stock pick for.
+      </Paragraph>
+      <Paragraph>
+        From the team at Weekly Stocktip, we wish you good luck in your future investment endeavors!
+      </Paragraph>
+      <Button onClick={onModalDismiss}>Return to My Account</Button>
+    </Container>
+  )
+}
 
 export default Success
