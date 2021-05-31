4e236c203e8be457e4cecc2079f09d5b179d2356
MarkLyck <mark.lyck@gmail.com>
2021-05-30 19:43:24 -0500

    :bug: fix icons flash big

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const withLess = require('@zeit/next-less')
+const withCSS = require('@zeit/next-css')
 const lessToJS = require('less-vars-to-js')
 const fs = require('fs')
 const path = require('path')
@@ -7,38 +8,40 @@ const path = require('path')
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, 'src/lib/assets/antd-custom.less'), 'utf8'))
 
-module.exports = withLess({
-  lessLoaderOptions: {
-    lessOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables, // make your antd custom effective
+module.exports = withCSS(
+  withLess({
+    lessLoaderOptions: {
+      lessOptions: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables, // make your antd custom effective
+      },
     },
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // generate sitemap
-      require('./scripts/generate-sitemap')
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        // generate sitemap
+        require('./scripts/generate-sitemap')
 
-      // ant design styles
-      const antStyles = /antd\/.*?\/style.*?/
-      const origExternals = [...config.externals]
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback()
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback)
-          } else {
-            callback()
-          }
-        },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-      ]
+        // ant design styles
+        const antStyles = /antd\/.*?\/style.*?/
+        const origExternals = [...config.externals]
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback()
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback)
+            } else {
+              callback()
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+        ]
 
-      config.module.rules.unshift({
-        test: antStyles,
-        use: 'null-loader',
-      })
-    }
-    return config
-  },
-})
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader',
+        })
+      }
+      return config
+    },
+  })
+)
