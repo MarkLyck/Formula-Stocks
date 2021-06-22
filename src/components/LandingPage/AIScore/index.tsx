1e49b4ca90665384e8911b18e222d4d62524584b
MarkLyck <mark.lyck@gmail.com>
2021-06-22 23:42:37 +0200

    :sparkles: update ai score text

--- src/components/LandingPage/AIScore/index.tsx
+++ src/components/LandingPage/AIScore/index.tsx
@@ -39,25 +39,25 @@ const AIScore = () => {
               The <Highlight>AI Score</Highlight>
             </ScalingTitle>
             <ScalingParagraph>
-              There are multiple ways to invest intelligently. You have previously met Joe, who is an artificial
-              intelligence specialized in selecting individual winning stocks.
+              There are multiple ways to invest intelligently. You have previously met Joe, who specializes in selecting
+              individual winning stocks.
               <br />
               <br />
-              Another approach is quantitative investing, in which you invest in a "basket" of stocks, say 20 or 40
-              stocks, which sports certain winning traits. These traits are calculated based on more than 200 different
-              mathematical factors, and we compute a score for each stock, called the AI-score, which is essentially a
-              number between -100 and 100, indicating its relative attractiveness to an investor.
+              Another approach is quantitative investing, where you invest in a "basket" of stocks, say 20 or 40 stocks,
+              which sports certain winning traits. These traits are derived from more than 200 different mathematical
+              and qualitative factors, and computed for each stock, called the AI-score. A number between -100 and 100,
+              indicating the relative attractiveness to an investor.
               <br />
               <br />
               Consider a stock with -100 as something to avoid, and a stock with an AI-score if 100 as something to
-              definitively invest in, and consider that all supported stocks receive a score.
+              absolutely invest in, and consider that all supported stocks receive a score.
               <br />
               <br />
-              If you for instance invest in 40 stocks with an AI-score of +80 a typical historical average return would
+              If you for instance invest in 40 stocks with AI-score {'>'} 80 a typical historical average return would
               be 27% pro anno<sup>*</sup>. See study for further details.
               <br />
               <br />
-              For each stock you get access to a detailed report, detailing risk vs. reward, value, growth,
+              For each stock you also get access to a detailed report, detailing risk vs. reward, value, growth,
               profitability, soundness and much more.
               <Disclaimer>
                 <sup>*</sup>Past performance does not guarantee future performance
