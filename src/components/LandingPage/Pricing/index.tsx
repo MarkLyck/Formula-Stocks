5af9f950475db0a4383bc56aaff3542cd6e4203c
MarkLyck <mark.lyck@gmail.com>
2021-07-03 14:10:38 +0200

    :sparkles: add annual pricing toggle

--- src/components/LandingPage/Pricing/index.tsx
+++ src/components/LandingPage/Pricing/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
 import { Divider, Button, Tooltip } from 'antd'
 import styled from '@emotion/styled'
 import { Element } from 'react-scroll'
 import { ScalingTitle, ScalingSubTitle, LandingPageContainer, Card, SmallFeatureCard } from 'src/ui-components'
 import theme from 'src/lib/theme'
+
+import ScheduleToggle from './ScheduleToggle'
 
 const CardTitle = styled.h4`
   margin: 0;
@@ -61,71 +63,94 @@ const CancelText = styled.p`
   text-align: center;
 `
 
-const Pricing = ({ showSignup }: any) => (
-  <LandingPageContainer align="center" marginBottom="4rem">
-    <Element name="pricing" />
-    <TitleContainer>
-      <ScalingTitle>Pricing</ScalingTitle>
-      <ScalingSubTitle>Our products are fine-tuned for your portfolio size.</ScalingSubTitle>
-    </TitleContainer>
-    <Container>
-      <PricingCard>
-        <CardTitle>Portfolio size under $100k</CardTitle>
-        <Divider />
-        <Tooltip title="Try it for free for 7-days">
-          <SmallFeatureCard style={{ boxShadow: 'none' }} icon="gift" color={theme.palette.success[600]} hover={false}>
-            Free 7-day trial
-          </SmallFeatureCard>
-        </Tooltip>
-        <PriceTag>
-          $49 / month<sup>*</sup>
-        </PriceTag>
-        <Button onClick={showSignup} style={{ fontWeight: 'bold' }} type="primary">
-          Try it for free
-        </Button>
-      </PricingCard>
-      <PricingCard>
-        <CardTitle>Portfolio size $100k - $1M</CardTitle>
-        <Divider />
-        <Tooltip title="This portfolio is tailored for medium-sized accounts.">
-          <SmallFeatureCard
-            style={{ boxShadow: 'none' }}
-            icon="analytics"
-            color={theme.palette.primary[600]}
-            hover={false}
-          >
-            Medium sized portfolios
-          </SmallFeatureCard>
-        </Tooltip>
-        <PriceTag>
-          $99 / month<sup>*</sup>
-        </PriceTag>
-        <Button onClick={showSignup} style={{ fontWeight: 'bold' }} type="primary">
-          Get started
-        </Button>
-      </PricingCard>
-      <PricingCard>
-        <CardTitle>Portfolio size over $1M</CardTitle>
-        <Divider />
-        <Tooltip title="Contact us for portfolios tailored for large accounts.">
-          <SmallFeatureCard
-            style={{ boxShadow: 'none' }}
-            icon="money-check-edit-alt"
-            color={theme.palette.warning[500]}
-            hover={false}
-          >
-            Large portfolios
-          </SmallFeatureCard>
-        </Tooltip>
-        <PriceTag>Contact us</PriceTag>
-        <Button style={{ fontWeight: 'bold' }}>Send email</Button>
-      </PricingCard>
-    </Container>
-    {/* <StyledAlert type="info" message="You stay in full control of your investments in your brokerage account!" /> */}
-    <CancelText>
-      <sup>*</sup>No lock-in contract, cancel anytime
-    </CancelText>
-  </LandingPageContainer>
-)
+const pricingMap = {
+  annually: {
+    entry: 49,
+    premium: 99,
+  },
+  monthly: {
+    entry: 59,
+    premium: 119,
+  },
+}
+
+const Pricing = ({ showSignup }: any) => {
+  const [term, setTerm] = useState<'annually' | 'monthly'>('annually')
+
+  return (
+    <LandingPageContainer align="center" marginBottom="4rem">
+      <Element name="pricing" />
+      <TitleContainer>
+        <ScalingTitle style={{ marginBottom: 16 }}>Pricing</ScalingTitle>
+        <ScheduleToggle term={term} setTerm={setTerm} />
+        <ScalingSubTitle>Our products are fine-tuned for your portfolio size.</ScalingSubTitle>
+      </TitleContainer>
+      <Container>
+        <PricingCard>
+          <CardTitle>Portfolio size under $100k</CardTitle>
+          <Divider />
+          <Tooltip title="Try it for free for 7-days">
+            <SmallFeatureCard
+              style={{ boxShadow: 'none' }}
+              icon="gift"
+              color={theme.palette.success[600]}
+              hover={false}
+            >
+              Free 7-day trial
+            </SmallFeatureCard>
+          </Tooltip>
+          <PriceTag>
+            ${pricingMap[term].entry} / month{term === 'annually' && <sup>*</sup>}
+          </PriceTag>
+          <Button onClick={showSignup} style={{ fontWeight: 'bold' }} type="primary">
+            Try it for free
+          </Button>
+        </PricingCard>
+        <PricingCard>
+          <CardTitle>Portfolio size $100k - $1M</CardTitle>
+          <Divider />
+          <Tooltip title="This portfolio is tailored for medium-sized accounts.">
+            <SmallFeatureCard
+              style={{ boxShadow: 'none' }}
+              icon="analytics"
+              color={theme.palette.primary[600]}
+              hover={false}
+            >
+              Medium sized portfolios
+            </SmallFeatureCard>
+          </Tooltip>
+          <PriceTag>
+            ${pricingMap[term].premium} / month{term === 'annually' && <sup>*</sup>}
+          </PriceTag>
+          <Button onClick={showSignup} style={{ fontWeight: 'bold' }} type="primary">
+            Get started
+          </Button>
+        </PricingCard>
+        <PricingCard>
+          <CardTitle>Portfolio size over $1M</CardTitle>
+          <Divider />
+          <Tooltip title="Contact us for portfolios tailored for large accounts.">
+            <SmallFeatureCard
+              style={{ boxShadow: 'none' }}
+              icon="money-check-edit-alt"
+              color={theme.palette.warning[500]}
+              hover={false}
+            >
+              Large portfolios
+            </SmallFeatureCard>
+          </Tooltip>
+          <PriceTag>Contact us</PriceTag>
+          <Button style={{ fontWeight: 'bold' }}>Send email</Button>
+        </PricingCard>
+      </Container>
+      {/* <StyledAlert type="info" message="You stay in full control of your investments in your brokerage account!" /> */}
+      {term === 'annually' && (
+        <CancelText>
+          <sup>*</sup>Paid annually
+        </CancelText>
+      )}
+    </LandingPageContainer>
+  )
+}
 
 export default Pricing
