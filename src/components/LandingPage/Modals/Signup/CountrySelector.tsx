06d6ab625057d2108130f0fe51d6c122d313833b
MarkLyck <mark.lyck@gmail.com>
2021-07-15 10:17:49 +0200

    :sparkles: country selector progress

--- src/components/LandingPage/Modals/Signup/CountrySelector.tsx
+++ src/components/LandingPage/Modals/Signup/CountrySelector.tsx
@@ -1,19 +1,47 @@
 import { Form, Select } from 'antd'
+import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import COUNTRY_OPTIONS from './countries'
 
-const CountrySelector = () => {
+const { Option } = Select
+
+const lookupIP = async () => {
+  const response = await fetch('https://extreme-ip-lookup.com/json/')
+  const json = await response.json()
+  return json
+}
+
+const CountrySelector = (props: any) => {
+  console.log('🔈 ~ props', props)
+  const [countryCode, setCountryCode] = useState('')
+  console.log('🔈 ~ countryCode', countryCode)
+  const getCountry = async () => {
+    const data = await lookupIP()
+    setCountryCode(data.countryCode)
+  }
+
+  const handleSelect = (value: string) => {
+    setCountryCode(value)
+  }
+
+  useEffect(() => {
+    getCountry()
+  }, [])
+
   return (
     <Form.Item name="country" rules={[{ required: true }]}>
-      <FontAwesomeIcon icon={['fad', 'globe-americas']} />
-      <Select
-        size="large"
-        mode="multiple"
-        placeholder="Country"
-        onChange={console.log}
-        options={COUNTRY_OPTIONS}
-        optionFilterProp="label"
-      />
+      <Select size="large" showSearch placeholder="Country" onChange={handleSelect} optionFilterProp="label">
+        {COUNTRY_OPTIONS.map((option) => (
+          <Option value={option.value} label={option.label}>
+            <div>
+              <span role="img" style={{ marginRight: 8 }}>
+                {option.emoji}
+              </span>
+              {option.label}
+            </div>
+          </Option>
+        ))}
+      </Select>
     </Form.Item>
   )
 }
